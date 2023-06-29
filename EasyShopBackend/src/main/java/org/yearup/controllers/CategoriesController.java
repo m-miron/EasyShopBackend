5c5a4bbe7dfb0df41c07694045d60e09cf308185
m-miron <mironmaricarmen@gmail.com>
2023-06-29 07:56:51 -0400

    Refactored error handling into addCategory method.

--- EasyShopBackend/src/main/java/org/yearup/controllers/CategoriesController.java
+++ EasyShopBackend/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,10 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -17,7 +19,7 @@ import java.util.List;
 @RequestMapping("/categories")
 // add the annotation to make this controller the endpoint for the following url
 // http://localhost:8080/categories
-@CrossOrigin()// Replace with the appropriate client URL
+@CrossOrigin// Replace with the appropriate client URL
 public class CategoriesController
 {
     private CategoryDao categoryDao;
@@ -33,6 +35,7 @@ public class CategoriesController
 
     // add the appropriate annotation for a get action
     @GetMapping
+    @PreAuthorize("permitAll()")
     public List<Category> getAll()
     {
         // find and return all categories
@@ -41,6 +44,7 @@ public class CategoriesController
 
     // add the appropriate annotation for a get action
     @GetMapping("/{id}")
+    @PreAuthorize("permitAll()")
     public Category getById(@PathVariable int id)
     {
         // get the category by id
@@ -50,6 +54,7 @@ public class CategoriesController
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
     @GetMapping("{categoryId}/products")
+    @PreAuthorize("permitAll()")
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
         // get a list of product by categoryId
@@ -58,18 +63,21 @@ public class CategoriesController
 
     // add annotation to call this method for a POST action
     @PostMapping
-    // add annotation to ensure that only an ADMIN can call this function
-    @Secured("ADMIN")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")// add annotation to ensure that only an ADMIN can call this function
     public Category addCategory(@RequestBody Category category)
     {
         // insert the category
-        return categoryDao.create(category);
+        try {
+            return categoryDao.create(category);
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
     @PutMapping
-    // add annotation to ensure that only an ADMIN can call this function
-    @Secured("ADMIN")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")// add annotation to ensure that only an ADMIN can call this function
     public void updateCategory(@PathVariable int id, @RequestBody Category category)
     {
         // update the category by id
@@ -80,8 +88,7 @@ public class CategoriesController
 
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
     @DeleteMapping
-    // add annotation to ensure that only an ADMIN can call this function
-    @Secured("ADMIN")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")// add annotation to ensure that only an ADMIN can call this function
     public void deleteCategory(@PathVariable int id)
     {
         // delete the category by id
