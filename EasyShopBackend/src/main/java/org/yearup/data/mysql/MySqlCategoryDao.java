4db5713fad2e3ae71ab9cab267899cf2a859e9c5
m-miron <mironmaricarmen@gmail.com>
2023-06-29 23:26:38 -0400

    Refactored MySqlCategoryDao exception handling and reporting. Exceptions now caught and wrapped in ResponseStatusException with HttpStatus codes. Also added handling of none updated or deleted rows under their corresponding methods.

--- EasyShopBackend/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ EasyShopBackend/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -1,51 +1,46 @@
 package org.yearup.data.mysql;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
-{
-    public MySqlCategoryDao(DataSource dataSource)
-    {
+public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
+    public MySqlCategoryDao(DataSource dataSource) {
         super(dataSource);
     }
 
     @Override
-    public List<Category> getAllCategories()
-    {
+    public List<Category> getAllCategories() {
         // get all categories
         List<Category> categories = new ArrayList<>();
-        try(PreparedStatement statement = getConnection().prepareStatement("""
-                    SELECT *
-                    FROM categories
-                    """);
-            ResultSet resultSet = statement.executeQuery()) {
-            while (resultSet.next()){
+        try (PreparedStatement statement = getConnection().prepareStatement("""
+                SELECT *
+                FROM categories
+                """);
+             ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
                 Category category = mapRow(resultSet);
                 categories.add(category);
             }
-        } catch (SQLException e){
-            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error occurred while retrieving categories.", e);
         }
         return categories;
     }
 
     @Override
     public Category getById(int categoryId) {
-
         try (PreparedStatement statement = getConnection().prepareStatement("""
                 SELECT *
-                FROM categories 
+                FROM categories
                 WHERE category_id = ?
                 """)) {
             statement.setInt(1, categoryId);
@@ -54,22 +49,21 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
                 if (resultSet.next()) {
                     return mapRow(resultSet);
                 } else {
-                    return null; // Category not found
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found.");
                 }
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error occurred while retrieving category.", e);
         }
     }
 
     @Override
-    public Category create(Category category)
-    {
+    public Category create(Category category) {
         // create a new category
-        try(PreparedStatement statement = getConnection().prepareStatement("""
-                    INSERT INTO categories (name, description)
-                    VALUES (?,?)
-                    """, Statement.RETURN_GENERATED_KEYS)) {
+        try (PreparedStatement statement = getConnection().prepareStatement("""
+                INSERT INTO categories (name, description)
+                VALUES (?,?)
+                """, Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, category.getName());
             statement.setString(2, category.getDescription());
 
@@ -82,20 +76,20 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
                     category.setCategoryId(categoryID);
                 }
             }
-        } catch (SQLException e){
-            throw new RuntimeException(e);
-        } return category;
+        } catch (SQLException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error occurred while creating category.", e);
+        }
+        return category;
     }
 
     @Override
-    public void update(int categoryId, Category category)
-    {
+    public void update(int categoryId, Category category) {
         // update category
         try (PreparedStatement statement = getConnection().prepareStatement("""
-            UPDATE categories
-            SET name = ?, description = ?
-            WHERE category_id = ?
-            """)) {
+                UPDATE categories
+                SET name = ?, description = ?
+                WHERE category_id = ?
+                """)) {
             statement.setString(1, category.getName());
             statement.setString(2, category.getDescription());
             statement.setInt(3, category.getCategoryId());
@@ -103,49 +97,43 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
             int rowsUpdated = statement.executeUpdate();
             if (rowsUpdated <= 0) {
                 // Handle the case where no rows were updated
-                throw new RuntimeException("No rows were updated for category ID: " + category.getCategoryId());
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found for update.");
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);// Handle the exception appropriately
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error occurred while updating category.", e);
         }
 
     }
 
     @Override
-    public void delete(int categoryId)
-    {
-        // Delete Category
-        try (PreparedStatement statement = getConnection().prepareStatement("""
-            DELETE FROM categories
-            WHERE category_id = ?
-            """)) {
+    public void delete(int categoryId) {
+        // delete category
+        String sql = "DELETE FROM Categories WHERE Category_Id = ?";
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, categoryId);
 
-            int rowsDeleted = statement.executeUpdate();
-            if (rowsDeleted <= 0) {
-                // Handle the case where no rows were deleted
-                throw new RuntimeException("No rows were deleted for category ID: " + categoryId);
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found.");
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
-            // Handle the exception appropriately
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error occurred during category deletion.", e);
         }
     }
 
-    private Category mapRow(ResultSet row) throws SQLException
-    {
+
+    private Category mapRow(ResultSet row) throws SQLException {
         int categoryId = row.getInt("category_id");
         String name = row.getString("name");
         String description = row.getString("description");
 
-        Category category = new Category()
-        {{
+        return new Category() {{
             setCategoryId(categoryId);
             setName(name);
             setDescription(description);
         }};
-
-        return category;
     }
 
 }
